f52d46ed7b329da6e6e70b6a8da7d4aaac5da110
Sujan Thapa <sujanthapamagar960@gmail.com>
2024-12-17 09:49:26 -0500

    starting node.js

---
+++ backend/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName = require('sillyname');
+// import generateName from "sillyname";
+// var sillyName = generateName();
+// console.log(`Silly name is ${sillyName}`)
+
+
+
+import superheros from "superheros"
+
+const name = superheros.random()
+console.log(name)
