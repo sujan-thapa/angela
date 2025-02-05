3eb41718f84723d89350f07b721a572aa2b482f1
Sujan Thapa <sujanthapamagar960@gmail.com>
2025-02-05 05:55:32 -0500

    random js

---
+++ react/sandbox/rgh.js
@@ -0,0 +1,34 @@
+const date = new Date();
+const year = date.getFullYear();
+const time = date.getHours();
+
+const ampm = time >= 12 ? 'PM' : 'AM';
+
+// const timeString = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: "numeric", hour12: true });
+// console.log(timeString)
+
+// let partOfTheDay = ""
+// if (time>=0&& time < 12) {
+//     partOfTheDay = "Good Morning"
+// }else if(time>=12&& time < 18){
+//     partOfTheDay = "Good Afternoon"
+// }else{
+//     partOfTheDay="Good Evening"
+// }
+// console.log(time)
+// console.log(ampm)
+// console.log(partOfTheDay)
+
+// let value = true
+// let sujan = "thapa"
+// console.log(sujan)
+
+// if (value) {
+//     let sujan = "vg"
+//     console.log(sujan)
+    
+// }else{
+//     console.log(sujan)
+// }
+// console.log(sujan)
+
