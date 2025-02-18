4ef0fce4eb0e92d8deaa54de9c9f183c4d2a0a9a
Sujan Thapa <sujanthapamagar960@gmail.com>
2025-02-18 06:23:02 -0500

    conditional rendering

---
+++ react/sandbox/13conditionalRendering/src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StrictMode } from 'react';
+
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+const root = createRoot(document.getElementById("root"))
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
+
+
+
+
