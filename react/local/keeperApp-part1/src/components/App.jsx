9b17501c1d94b6aba8cd925a151b732696c88af7
Sujan Thapa <sujanthapamagar960@gmail.com>
2025-02-18 06:22:03 -0500

    Keeper App update

--- react/local/keeperApp-part1/src/components/App.jsx
+++ react/local/keeperApp-part1/src/components/App.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer"
+// import notes from "./Note";
+
+import notes from "../note";
 import Note from "./Note";
 
+function note(notee) {
+    return (
+        <Note
+            key={notee.key}
+            title={notee.title}
+            content={notee.content}
+        />
+    )
+}
 
-function App(){
+function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {/* <Note /> */}
+
+            {notes.map(note)}
+
             <Footer />
 
         </div>
