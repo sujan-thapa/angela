8286ce6e3f5544ee2a3665230a63e075bc9b4ed9
Sujan Thapa <sujanthapamagar960@gmail.com>
2025-02-19 12:37:33 -0500

    Conditional Rendering

---
+++ react/sandbox/13conditionalRendering/src/components/App1.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Form from "./Form";
+
+var userIsRegistered = true;
+
+function App1() {
+  return <Form isRegistered={userIsRegistered} />;
+}
+
+export default App1;
--- react/sandbox/13conditionalRendering/src/index.jsx
+++ react/sandbox/13conditionalRendering/src/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { StrictMode } from 'react';
 
 import { createRoot } from "react-dom/client";
-import App from "./components/App";
+// import App from "./components/App";
+import App1 from "./components/App1";
+
 
 const root = createRoot(document.getElementById("root"))
 root.render(
     <StrictMode>
-        <App />
+        <App1 />
     </StrictMode>
 )
 
